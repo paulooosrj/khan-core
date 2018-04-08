1efaf6bdf19c266aafe54fcb28d727b6fb1ca87c
PaulaoDev <jskhanframework@gmail.com>
2018-04-08 00:53:12 -0300

    arrumei

--- src/command/GulpCommand.php
+++ src/command/GulpCommand.php
@@ -31,8 +31,8 @@
 	    protected function execute(InputInterface $input, OutputInterface $output){
 
 	    	if(
-				copy('.cli/gulp-files/file-gulpfile.js', 'gulpfile.js') && 
-				copy('.cli/gulp-files/file-package.json', 'package.json')
+				copy(__DIR__.'/../../.cli/gulp-files/file-gulpfile.js', 'gulpfile.js') && 
+				copy(__DIR__.'/../../.cli/gulp-files/file-package.json', 'package.json')
 			){
 				KhanCommand::shell("npm i gulp -g && npm install");
 				$this->sucess("Gulp configurado com sucesso");
