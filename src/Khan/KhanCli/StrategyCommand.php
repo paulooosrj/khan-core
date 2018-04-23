1b16507a739d3c5efe0fe9169d27829cf313817c
PaulaoDev <jskhanframework@gmail.com>
2018-04-22 21:26:53 -0300

    update stra

--- src/Khan/KhanCli/StrategyCommand.php
+++ src/Khan/KhanCli/StrategyCommand.php
@@ -39,7 +39,7 @@
 
 				$make2 = Hooks::write($strategyOrigin, 
 					Hooks::replace($strategyOrigin, [
-						"// newStrategy" => "'$strategy' => 'StrategysAuth\\".$strategy."'
+						"// newStrategy" => ",'$strategy' => 'StrategysAuth\\".$strategy."'
 		// newStrategy"
 				]));
 
