b6e8c847b36a6a3530655b51c090987a1d866c17
Paulao Developer <jskhanframework@gmail.com>
2018-04-29 10:30:39 -0300

    Update strategy.php

--- src/Khan/KhanCli/storage/strategy/strategy.php
+++ src/Khan/KhanCli/storage/strategy/strategy.php
@@ -3,24 +3,36 @@
 	namespace StrategysAuth;
 
 	class strategyName {
-
 		// Table mysql using
 		const table = "login";
-
 		// Route logout
-		const logout = "./login";
-
+		const logout = "../login";
 		// Ignore key init session
 		const ignoreSession = ["password"];
-
+		// login inputs
 		const login = [
 			// fields POST and COLUMNS
 			"email", "password"
 		];
-
+		// register inputs and register in mysql
 		const register = [
 			// fields POST and COLUMNS
 			"email", "password", "name"
 		];
-
+		// extends functions
+		public static function extendsLogin($data){
+			// $data["email"] = strip_tags($data["email"]);
+			return $data;
+		}
+		public static function extendsRegister($data){
+			// $data["password"] = md5($data["password"]);
+			return $data;
+		}
+		public static function extendsLogout(){
+			// echo "Saiu";
+		}
+		public static function extendsValidate($data){
+			// $data["password"] = md5($data["password"]);
+			return $data;
+		}
 	}
